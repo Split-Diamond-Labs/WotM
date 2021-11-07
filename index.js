13bf95aaabaae27fca083677c7cf68bea015d07a
Flyspeck101 <yeetersdeleters12345@gmail.com>
2021-11-07 13:36:34 +1100

    Update index.js

--- index.js
+++ index.js
@@ -14,26 +14,23 @@ function refresh() {
   let DOMString = "";
   
   for (const item in rooms[currentRoom].items) {
-    DOMString += `<br>
-    <br>
+    DOMString += `
     <button id="${item}" onclick='(function(e) { rooms[currentRoom].items["${item}"].onGrab(); inventory["${item}"] = rooms[currentRoom].items["${item}"]; delete rooms[currentRoom].items["${item}"]; refresh(); notify(\`You picked up the ${rooms[currentRoom].items[item].name}\`); })(this)'>Take ${rooms[currentRoom].items[item].name}</button>
     `;
   }
   
-  DOMString += "<br>";
+  DOMString += "<br><br>";
   
   for (const item in inventory) {
-    DOMString += `<br>
-    <br>
+    DOMString += `
     <button id="${item}" onclick='(function(e) { inventory["${item}"].onDrop(); rooms[currentRoom].items["${item}"] = inventory["${item}"]; delete inventory["${item}"]; refresh(); notify(\`You dropped your ${inventory[item].name}\`); })(this)'>Drop ${inventory[item].name}</button>
     `;
   }
   
-  DOMString += "<br>";
+  DOMString += "<br><br>";
   
   for (const path in rooms[currentRoom].exits) {
-    DOMString += `<br>
-    <br>
+    DOMString += `
     <button id=${path} onclick="exit('${path}')">Go ${path}${rooms[currentRoom].exits[path].locked ? " (locked)" : ""}</button>`;
   }
   
